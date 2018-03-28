76aa43965ef82fd8d7adbfd325bef10051242a74
Frank Liao <recs7168@gmail.com>
2018-03-27 22:33:58 -0400

    examples in file

--- app/models/AutomatedLogic.js
+++ app/models/AutomatedLogic.js
@@ -3,17 +3,50 @@ let MongoModel = require( require('path').resolve() /*+ '/app/models/MongoRecord
 class AutomaticLogicRecord extends MongoModel { }
 AutomaticLogicRecord.collection_name = 'AutomatedLogic';
 
+
+// DEFINING AVAILABLE DATA
+Sun {
+	"solar_energy_generated": {
+		hour: kWh,
+		hour: kWh,
+		...
+	},
+	"solar_radiation": {
+		data....
+	},
+}
+
+
 // Automated Logic class to be imported in other places
 class AutomatedLogic {
 	constructor() {
 
 	}
-	getWeatherData(callback) {
-
-	}
 	getSunData(callback) {
+		// example life cycle
+		this._retrieveSunData(function(data) {
+			let parsed_data = this._parseSunData(data);
+			let sun = this._formatSunData(parsed_data);
+			callback(sun);
+		});
+	}
+	// retrieves raw data from AutomatedLogic
+	_retrieveSunData(callback) {
 		
 	}
+	// parse raw data that was retrieved
+	_parseSunData(data) {
+
+	}
+	// formats parsed data
+	_formatSunData(data) {
+		let sun = {
+
+		};
+		// fill up with defined information
+		return sun;
+	}
+
 	getWaterData(callback) {
 
 	}
@@ -21,6 +54,11 @@ class AutomatedLogic {
 
 	}
 }
+
+// example controller calls to AutomatedLogic.js
+// automated_logic.getSunData(function(sun) {
+// 	socket.emit('solar_energy', sun['solar_energy_generated'])
+// });
 
 let automated_logic = new AutomatedLogic();
 // TESTS GO HERE
