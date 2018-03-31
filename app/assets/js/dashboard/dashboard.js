39ac15d06db13932b60e914e16ffd619eb664be0
Frank Liao <recs7168@gmail.com>
2018-03-31 06:55:24 -0400

    widget_list and toast_list
    - toast_list [transitions] and toastAlert() implemented
    - widget deletion implemented
    - widget_list transitions implemented

--- app/assets/js/dashboard/dashboard.js
+++ app/assets/js/dashboard/dashboard.js
@@ -1,14 +1,21 @@
 // DEFINE DASHBOARD SOCKET EVENT
+let	_addWidget;
+let current_category = 'weather';
 (function() {
-	let current_category = 'weather';
-
 	socket.on('get_widgets', (data) => {
 		let	category = data.category,
 			widgets = data.widgets;
 		if (!widgets && category == current_category) {
-			alert('no data could be found');
+			alertToat('failure', 'no data could be found');
 		} else if (category == current_category) {
-			widget_list.widgets = widgets;
+			for (let i = 0; i < widget_list.widgets.length; i++) {
+				setTimeout(() => {
+					widget_list.widgets.pop();
+				}, 0);
+			}
+			setTimeout(() => {
+				widget_list.widgets = widgets;
+			}, 0);
 		}
 	});
 	// on update 
@@ -16,10 +23,10 @@
 		let	category = data.category,
 			widgets = data.widgets;
 		if (!widgets && category == current_category) {
-			alert('widget list failed to update');
+			alertToast('failure', 'widget list failed to update');
 		}
 		else if (category == current_category) {
-			alert('widget list successfully updated');
+			alertToast('success', 'widget list successfully updated');
 			widget_list.widgets = widgets;
 		}
 	});
@@ -43,7 +50,52 @@
 			category: current_category
 		});
 	}
+
+	function addWidget(widget, index) {
+		const required_fields = ['data_label', 'content_type'];
+		for (let i = 0; i < required_fields.length; i++) {
+			let field = required_fields[i];
+			if (!widget[field]) {
+				alertToast('failure', 'widget is missing information');
+				return;
+			}
+		}
+		let	widgets = widget_list.widgets,
+			new_widgets = widgets.slice(0);
+		// console.log(new_widgets);
+		if (index) new_widgets.splice(index, 0, widget);
+		else new_widgets.push(widget);
+
+		socket.emit('update_widgets', {
+			category: current_category,
+			widgets: new_widgets
+		});
+	}
+
+	function removeWidget(index) {
+		let	widgets = widget_list.widgets,
+			new_widgets = widgets.slice(0);
+		if (!(index >= 0 && index <= widgets.length)) return;
+
+		new_widgets.splice(index, 1);
+
+		socket.emit('update_widgets', {
+			category: current_category,
+			widgets: new_widgets
+		});
+	}
+
+	_addWidget = addWidget;
+	_removeWidget = removeWidget;
 })();
+
+function alertToast(type, message) {
+	toast_list.toasts.push({
+		type: type,
+		message: message,
+		created_at: new Date().getTime()
+	});
+}
 
 // weather activation functions
 function activateWeather(weather) {
